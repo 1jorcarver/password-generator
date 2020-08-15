947c358cab79b0d502d8b9471cbb61179747b3b6
Jordan Carver <jordan_carver@hotmail.com>
2020-08-15 16:21:46 -0600

    set up framework for password generator

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 // Assignment code here
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var passwordEmpty = []; 
+// Array.toString()
+// String.replace()
+var totalArray = [];
 
+// Prompt user about length of password
+function promptLength(){
+ var length = prompt("How long do you want your password from 8 to 128 characters?")
+ 
+ if (length does not meet conditions){
+   alert("Password length not valid. Must be 8-128 characters.");
+   promptLength();
+  }
+}
+
+promptLength();
+
+// Asking user to confirm the other variables
+var useUpper = confirm("Do you want to use an uppercase letter?")
+// IF USER SAYS YES
+totalArray.push(uppercase)
+
+
+
+// Password function
+// var i = 0
+function generatePassword(){
+// grab the i-th array from total array
+// pick random index between 0 and totalarray[i].length
+// push that character to passwordempty
+
+// if passwordempty.length is shorter than length
+// i++
+// generatepassword()
+// if password is long enough: return result(whatever you want that result to be)
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
