dae7670f6d8d7bc6d04a27aae0442dd207937e51
Jordan Carver <jordan_carver@hotmail.com>
2021-01-20 21:48:53 -0700

    remodified code, working now

--- README.md
+++ README.md
@@ -1,26 +1,22 @@
-# Password Generator Starter Code
+# Password Generator
 
-## Motivation
-My motivation was to create a password generator which produced secure passwords.
+## Description
+A password generator which produces secure passwords.
+
+## Screenshot
+![Screenshot](screenshot.png)
+
+## Link to Live URL
+https://1jorcarver.github.io/password-generator/
 
 ## Website Languages
-I used HTML, CSS, and JavaScript.
+HTML, CSS, and JavaScript
 
 ## Intended Website Functionality
 - The user is able to create a secure password from 8 to 128 characters long. 
 - The password contains lower and upper case letters, special characters, and numbers.
 - Once the user has followed the prompts and met all of the criteria, the website populates the password, writing it on the screen.
 
-## Problems
-- The website is not functional. But, happy to say I attempted. #Work-in-Progress!
-- Prompts are not working.
-- My arrays are a jumbled-mess and need more meaningful names that correlate to what they do.
-- I attempted to use, for my arrays, toString and .replace. This was much more of a challenge than anticipated.
-- I need to clean up my initial design, note writing, and clean up the syntax.
-
 ## Contribution
 - I received assistance from the TA and used a lot of MDN and other tutorials/websites. 
-
-## Dreams
-- I hope to understand this JS stuff! :-)
 
--- script.js
+++ script.js
@@ -1,69 +1,53 @@
 // Create arrays which lists out all of the options
-var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specArray = ["!", "@", "#", "$", "%", "^", "&", "*"];
-var pwArray = []; 
-// Array.toString()
-var ps = totalArray.toString();
-// String.replace()
-var removeCommas = /,/gi;
-var pwText = totalArray.replace(removeCommas, "");
-
-var totalArray = [];
-
-// Prompt user about length of password
-function promptLength(){
- var length = prompt("How long do you want your password to be? Must be 8-128 characters.");
- 
- if (!length){
-   alert("Password length not valid. Must be 8-128 characters.");
-   promptLength();
-  }
-};
-
-promptLength();
-
-// Asking user to confirm the other variables
-var useUpper = confirm("Do you want to use an uppercase letter?");
-// IF USER SAYS YES
-totalArray.push(upperArray);
-
-var useLower = confirm("Do you want to use a lowercase letter?");
-
-totalArray.push(lowerArray);
-
-var useNum = confirm("Do you want to use a number?");
-
-totalArray.push(numArray);
-
-var useSpec = confirm("Do you want to use a special character?");
-
-totalArray.push(specArray);
-
-
+var lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specArray = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', ' ', "\\"];
 
 // Password function
-// var i = 0
-var i = 0;
-function generatePassword(){
-// grab the i-th array from total array
-  totalArray.charAt();
-// pick random index between 0 and totalarray[i].length
-  var randomChoice = totalArray[Math.floor(Math.random() * totalArray[i].length)];
-// push that character to passwordempty
-  totalArray.push(pwArray);
-// if passwordempty.length is shorter than length
-  if (pwArray.length < length) {
-    var value = value[Math.floor(Math.random() * totalArray[i].length) + i++];
+var generatePassword = function () {
+  var length = window.prompt("How long do you want your password to be? Must be 8-128 characters.");
+  length = parseInt(length);
+  
+  if (length >= 8 && length <= 128) {
+    var useLower = window.confirm("Do you want to use a lowercase letter?");
+    var useUpper = window.confirm("Do you want to use an uppercase letter?");
+    var useNum = window.confirm("Do you want to use a number?");
+    var useSpec = window.confirm("Do you want to use a special character?");
+    
+    if (useLower === false && useUpper === false && useNum === false && useSpec === false) {
+      window.alert("Please select a character type.")
+      generatePassword();
+    }
+    
+    //variable for the new password
+    var newPw = "";
+    for (var i = 0; i < length; i) {
+
+      if (useLower === true) {
+        newPw += upperArray[Math.floor(Math.random() * 26)];
+        i++;
+      }
+      if (useUpper === true) {
+        newPw += numArray[Math.floor(Math.random() * 26)];
+        i++;
+      }
+      if (useNum === true) {
+        newPw += numArray[Math.floor(Math.random() * 10)];
+        i++;
+      }
+      if (useSpec === true) {
+        newPw += specArray[Math.floor(Math.random() * 33)];
+        i++;
+      }
+    }
   }
-// i++
-  generatePassword();
-// if password is long enough: return result(whatever you want that result to be)
-  if (pwArray.length >= length){
-  return "Your password creation was a success!"
+  else {
+    window.alert("Please use a number between 8 and 128");
+    generatePassword();
   }
-};
+  return newPw;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
